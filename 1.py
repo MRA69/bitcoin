517c6baf72b332193770b4b29faa94729ce93793
MRA69 <58457934+MRA69@users.noreply.github.com>
2021-06-15 02:30:33 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,18 @@
 import pandas as pd
 df = pd.read_csv('mempool.csv')
 df.columns =[column.replace(" ", "_") for column in df.columns]
-df = df[df['parents_'] >= ' ']
-print(df['parents_'])
-kf = df.head(2000)
+sf = df[df['parents_'] >= ' ']
+print(sf['parents_'])
+kf = sf.head(2000)
 print(kf['tx_id'])
+
+"""
+***THIS IS THE ACTUAL LONG VERSION WHICH IS TOTALLY NOT CLEAR ON TERMINAL******
+import pandas as pd
+df = pd.read_csv('mempool.csv')
+df.columns =[column.replace(" ", "_") for column in df.columns]
+sf = df[df['parents_'] > ' ']
+print(sf['parents_'].to_string())
+kf = sf.head(2000)
+print(kf['tx_id'].to_string())
+"""
